21cf8259befdb9a5777e820154cc8839f69e18ed
KaruptsockTheRealOne <90372126+KaruptsockTheRealOne@users.noreply.github.com>
2025-01-14 12:54:52 +0800

    Update Loader.lua

--- saved games/dcs/pg/src/Loader.lua
+++ saved games/dcs/pg/src/Loader.lua
@@ -1,17 +1,17 @@
 -- Globals are important.
 version = "1.110.0"
-Servername = "Persia Aflame"
+Servername = "Persia ABlaze"
 lastupdate = "21/10/2022"
 password = "lalalala"
 ADMINPASSWORD2 = "lala"
-PATHTODROPBOX = "C:\\Users\\root\\Dropbox\\ServerShared\\Persia_Aflame_Core_Code\\dcs\\" -- path to were the files are this needs to go in the right folder
-usedropbox = true
+PATHTODROPBOX = "C:\\%Users%\\Persia_ABlaze_Core_Code\\dcs\\" -- path to were the files are this needs to go in the right folder
+usedropbox = false
 PGPATH = lfs.writedir()
 if usedropbox == true then
 	PGPATH = PATHTODROPBOX
 end
 -- end globals.
-env.info("Persia Aflame Loader")
-env.info("Mission by Robert Graham")
+env.info("Persia ABlaze Loader")
+env.info("Mission by Karuptsock")
 env.info("Mission Script Loader Active")
 dofile(PGPATH .."pg\\scripts\\Main.lua")
