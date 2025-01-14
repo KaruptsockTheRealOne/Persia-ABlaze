9d596c4de67d5b9e4bad0b5f7a136dcadeea1349
KaruptsockTheRealOne <90372126+KaruptsockTheRealOne@users.noreply.github.com>
2025-01-14 11:32:14 +0800

    Update dedicatedServer.lua

--- serversidescripts/Scripts/dedicatedServer.lua
+++ serversidescripts/Scripts/dedicatedServer.lua
@@ -1,4 +1,4 @@
-net.set_name('TGW Server')
+net.set_name('ABlaze Server')
 local res = net.start_server(serverSettings)
 if res ~= 0 then
     log.write('Dedicated Server', log.DEBUG, 'Failed to start server with code: ', res)
